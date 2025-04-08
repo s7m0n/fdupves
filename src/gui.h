5cdde4d8de16c8919f7b2f6cad5ca84c9683a2c0
Alf <naihe2010@126.com>
2025-04-08 21:51:52 +0800

    Use idle func to scroll to origin value

--- src/gui.h
+++ src/gui.h
@@ -32,45 +32,47 @@
 
 typedef struct file_node_s file_node;
 
-struct gui_s {
-    gboolean quit;
+struct gui_s
+{
+  gboolean quit;
 
-    GtkWidget *widget;
-    GtkWidget *main_vbox;
-    GtkWidget *progress;
+  GtkWidget *widget;
+  GtkWidget *main_vbox;
+  GtkWidget *progress;
 
-    GtkToolItem *but_add;
-    GtkToolItem *but_find;
-    GtkToolItem *but_del;
+  GtkToolItem *but_add;
+  GtkToolItem *but_find;
+  GtkToolItem *but_del;
 
-    GtkListStore *dir_store;
+  GtkListStore *dir_store;
 
-    GPtrArray *images;
-    GPtrArray *videos;
-    GPtrArray *audios;
-    GPtrArray *ebooks;
-    GSList *same_images;
-    GSList *same_videos;
-    GSList *same_audios;
-    GSList *same_ebooks;
-    GSList *same_list;
+  GPtrArray *images;
+  GPtrArray *videos;
+  GPtrArray *audios;
+  GPtrArray *ebooks;
+  GSList *same_images;
+  GSList *same_videos;
+  GSList *same_audios;
+  GSList *same_ebooks;
+  GSList *same_list;
 
-    GtkWidget *log_tree;
-    GtkListStore *log_store;
+  GtkWidget *log_tree;
+  GtkListStore *log_store;
 
-    GtkWidget *result_tree;
-    GtkTreeStore *result_store;
-    GtkTreeSelection *result_select;
-    GtkTreeIter *result_select_iters;
-    file_node **result_file_nodes;
+  GtkWidget *result_tree;
+  GtkTreeStore *result_store;
+  GtkTreeSelection *result_select;
+  GtkTreeIter *result_select_iters;
+  file_node **result_file_nodes;
+  gdouble scroll_value;
 
-    GAsyncQueue *step_queue;
-    GAsyncQueue *log_queue;
-    guint queue_timer;
+  GAsyncQueue *step_queue;
+  GAsyncQueue *log_queue;
+  guint queue_timer;
 };
 
 typedef struct gui_s gui_t;
 
-gboolean gui_init(int argc, char *argv[]);
+gboolean gui_init (int argc, char *argv[]);
 
 #endif
